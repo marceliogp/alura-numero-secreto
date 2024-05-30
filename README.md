fe3920bd32fc6025d1f84105f5d160a4905b12b9
Marcélio G. Pereira <marcelio.pereira@tse.jus.br>
2024-05-30 11:59:36 -0300

    Aula03 - Concluído aula 03

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Projeto do curso de Lógica de Programação da Alura
 
 - Aula 01 - Iniciando com Javascript.
 - Aula 02 - Condicionais e concatenação.
+- Aula 03 - Loops e tentativas.
--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute;
+let tentativas=1;
 
-// Adicione um console.log para verificar o valor de "chute" após a entrada do usuário
-let chute = prompt('Escolha um número entre 1 e 10');
-console.log('Valor do chute:', chute);
-
-let numeroSecreto = 4;
-
-// Adicione um console.log para verificar a comparação entre "chute" e "numeroSecreto"
-console.log('Resultado da comparação:', chute == numeroSecreto);
-
-if (chute == numeroSecreto) {
-    alert('Acertou');
-} else {
-    // Adicione um console.log para verificar o valor de "numeroSecreto" quando o jogador erra
-    console.log('Valor do número secreto:', numeroSecreto);
-    alert('O número secreto era ' + numeroSecreto);
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa(s).`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+    }
 }
